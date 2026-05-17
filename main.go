da36aa592d46efeeaac8491919193658a2181020
khurram <khurram.m.ilyas@gmail.com>
2026-05-17 18:09:19 +0200

    commit message 3

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"introduction/calc"
 )
 
+func MainMain() int {
+	return 2000
+}
+
 func main() {
 	defer fmt.Println("deffer the running of this print...")
 	fmt.Println("Main is running...")
 	fmt.Println(calc.CalcCustom())
 
-	fmt.Append(calc.Newfunction1())
+	fmt.Println(calc.Newfunction1())
 
-	fmt.Append(calc.Newfunction2())
+	fmt.Println(calc.Newfunction2())
 }
